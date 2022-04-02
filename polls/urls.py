cf47590b437726f72167b90ad5e56b076b17e9b7
pooja-varmaa <varmapooja020@gmail.com>
2022-04-02 20:39:08 +0530

    initial commit

---
+++ polls/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path
+  
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('', views.index, name='index'),
+]
+
+
+
+
+
+
+      
